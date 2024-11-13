26d9c88adfccb7fd41e113e946b76de35cfc7c94
Varsha S <varshasuresh447@gmail.com>
2024-11-13 17:23:10 +0530

    Update models.py

--- apibackendapp/models.py
+++ apibackendapp/models.py
@@ -4,7 +4,7 @@ from django.db.models.signals import post_save
 from django.conf import  settings  
 from rest_framework.authtoken.models import Token   
 
-
+# my dear
 # Create your all wonderful models here.
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
